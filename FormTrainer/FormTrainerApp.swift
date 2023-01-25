924424edd74f4da2a1992cfdfb987c5137fb8f2c
kazua33 <21cm0133@jec.ac.jp>
2023-01-25 10:04:32 +0900

    Initial Commit

---
+++ FormTrainer/FormTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormTrainerApp.swift
+//  FormTrainer
+//
+//  Created by 稗田一亜 on 2023/01/25.
+//
+
+import SwiftUI
+
+@main
+struct FormTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
